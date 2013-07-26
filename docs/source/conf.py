1c644531ad04804acbfcd0636c75f25bb6cc0e85
Rob Ruana <rob@relentlessidiot.com>
2013-07-26 04:10:27 -0400

    Attempt to get readthedocs.org working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..')))
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
