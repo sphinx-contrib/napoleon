7857923651a8df9d38513b2416c4619284bc55e0
Rob Ruana <rob@relentlessidiot.com>
2013-07-26 03:50:07 -0400

    Attempt to get readthedocs.org working

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,16 @@ import sys
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-# sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+    os.chdir('napoleon')
+else:
+    html_theme = 'armstrong'
+    html_theme_path = ['_themes']
+
 
 # -- Options for Napoleon Extension --------------------------------------------
 
@@ -154,7 +163,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # html_theme = 'default'
-html_theme = 'armstrong'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -167,7 +175,6 @@ html_theme = 'armstrong'
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
-html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
