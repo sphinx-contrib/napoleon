811a16ca22700ebea50d1e18de918b0326fc9867
Rob Ruana <rob@relentlessidiot.com>
2015-01-12 16:52:56 -0500

    Bumps napoleon version number to 0.2.9

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.version_info[0] >= 3:
 
 setup(
     name='sphinxcontrib-napoleon',
-    version='0.2.8',
+    version='0.2.9',
     url='https://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-napoleon',
     license='BSD',
