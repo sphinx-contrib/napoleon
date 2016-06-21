a520c6358b535758cf081bf64b605eb1ba225b17
John Hagen <johnthagen@gmail.com>
2016-06-21 18:29:30 -0400

    Fix extraneous newline.

--- docs/source/example_google.py
+++ docs/source/example_google.py
@@ -5,7 +5,6 @@ This module demonstrates documentation as specified by the `Google Python
 Style Guide`_. Docstrings may extend over multiple lines. Sections are created
 with a section header and a colon followed by a block of indented text.
 
-
 Example:
     Examples can be given using either the ``Example`` or ``Examples``
     sections. Sections support any reStructuredText formatting, including
